b233671a1af97e5e89fc4c8ea59399711c21276e
Anil Patel <Anil.Patel@knightfrank.com>
2023-12-17 22:44:32 +0000

    add model class

---
+++ src/AzureAIContentSafety/ContentSafety/Models/SeverityModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureAIContentSafety.ContentSafety.Models
+{
+    public class SeverityModel
+    {
+        public string CategoryName { get; set; }
+        public int SeverityLevel { get; set; }
+    }
+}
